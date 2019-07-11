ff5fb536b0dc7115abeb5ad7c4eef9cf2c09c2e6
Mathilde <math.1510@hotmail.fr>
2019-07-11 19:08:58 +0200

    recraftor jwt utils

--- src/Utils/JwtUtils.php
+++ src/Utils/JwtUtils.php
@@ -20,14 +20,8 @@ class JwtUtils
 
     public function decodeToken(string $token) : array
     {
-        $validToken = $this->parseBearer($token);
-
-        if($validToken == null){
-            return array('error' => 'No bearer token');
-        }
-
         try {
-            $result = JWT::decode($validToken, $_ENV['JWTKEY'], array('HS256'));
+            $result = JWT::decode($token, $_ENV['JWTKEY'], array('HS256'));
         } catch (\Exception $e) {
             return array('error' => 'Invalid token');
         }
@@ -36,16 +30,5 @@ class JwtUtils
         return (array) $result;
     }
 
-    private function parseBearer(string $token) : ?string
-    {
-        $bearerIndex = strpos($token, 'Bearer');
-
-        if(strpos($token, 'Bearer') === false){
-            return null;
-        }
-
-        // token without the word Bearer (6 is bearer word length) and extra space
-        return trim(substr($token, 6));
-    }
 
 }
